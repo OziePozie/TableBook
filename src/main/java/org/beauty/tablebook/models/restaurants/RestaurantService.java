dc3bbf14b404804ee3287b717381cf2b71e7f263
timka <timkaa555@gmail.com>
2024-01-30 09:43:01 +0300

    add service

---
+++ src/main/java/org/beauty/tablebook/models/restaurants/RestaurantService.java
@@ -0,0 +1,34 @@
+package org.beauty.tablebook.models.restaurants;
+
+import org.beauty.tablebook.models.users.Users;
+import org.beauty.tablebook.models.users.UsersRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RestaurantService {
+
+    private final RestaurantsRepository restaurantRepository;
+    private final UsersRepository userRepository;
+
+    public RestaurantService(RestaurantsRepository restaurantRepository, UsersRepository userRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void saveRestaurant(RestaurantDTO restaurantDTO) {
+
+        Optional<Users> optionalUser = userRepository.findById(restaurantDTO.getOwnerID());
+
+        if (optionalUser.isPresent()) {
+            Users owner = optionalUser.get();
+
+            Restaurants restaurant = restaurantDTO.fromDTOtoEntity();
+            restaurant.setOwner(owner);
+            restaurantRepository.save(restaurant);
+        } else {
+            throw new RuntimeException("User not found with ID: " + restaurantDTO.getOwnerID());
+        }
+    }
+}
