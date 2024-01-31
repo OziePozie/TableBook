10c760ebaf64c9c91c7a0e7dad5d5b65bcbf660c
timka <timkaa555@gmail.com>
2024-01-31 11:07:34 +0300

    add postrequst for tables

---
+++ src/main/java/org/beauty/tablebook/controllers/restaurant/requests/PostTablesRequestDTO.java
@@ -0,0 +1,13 @@
+package org.beauty.tablebook.controllers.restaurant.requests;
+
+import lombok.Getter;
+import org.beauty.tablebook.models.tables.TableDTO;
+import org.beauty.tablebook.models.tables_version.TablesVersion;
+
+import java.util.List;
+@Getter
+public class PostTablesRequestDTO{
+    String versionName;
+    List<TableDTO> tableDTOList;
+
+}
